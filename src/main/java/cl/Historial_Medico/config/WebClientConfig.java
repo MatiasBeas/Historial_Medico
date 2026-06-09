3bf766df9153873f6737a0f99b86e10233b7cb3a
Matías <ma.beas@duocuc.cl>
2026-06-08 23:30:25 -0400

    Lo mismo que antes menos seguridad

--- src/main/java/cl/Historial_Medico/config/WebClientConfig.java
+++ src/main/java/cl/Historial_Medico/config/WebClientConfig.java
@@ -1,16 +1,20 @@
 package cl.Historial_Medico.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+    @Value("${webclient.paciente.url}")
+    private String pacienteUrl;
+
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8082")
+                .baseUrl(pacienteUrl)
                 .build();
     }
-
 }
